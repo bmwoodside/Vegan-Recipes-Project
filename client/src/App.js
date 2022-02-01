a70ba5c823bbafdd6eb319697c008a62351d0876
JohnPaul-Londono <johnplondono@gmail.com>
2022-02-01 16:31:13 -0600

    added nav

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Main from './views/Main';
+import Nav from './views/Nav';
 
 
 function App() {
   return (
     <BrowserRouter>
+    <Nav />
       
       <Route exact path="/">
         <Main />
---
+++ client/src/views/Nav.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+const Nav = (props) => {
+
+
+return(
+    <div className='nav'>
+        hi bitches
+    </div>
+)
+
+}
+
+export default Nav;
